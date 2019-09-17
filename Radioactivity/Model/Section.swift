d38439f1ff6f2b241668869980f1488a27015293
Jacob Clayden <jacobcxdev@gmail.com>
2019-09-17 11:37:47 +0100

    Removed unnecessary code

--- Radioactivity/Model/Section.swift
+++ Radioactivity/Model/Section.swift
@@ -8,7 +8,7 @@
 
 import SwiftUI
 
-class Section: Identifiable, Codable {
+class Section: Identifiable {
     let id: Int
     
     var title: String = ""
@@ -30,37 +30,5 @@ class Section: Identifiable, Codable {
         self.imageURL = imageURL
         self.imageBackground = imageBackground
         self.uiimage = uiimage
-    }
-    
-    enum CodingKeys: String, CodingKey {
-        case id = "id"
-        case title = "title"
-        case description = "description"
-        case content = "content"
-        case imageURL = "imageURL"
-        case imageBackground = "imageBackground"
-        case uiimage = "uiimage"
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(id, forKey: .id)
-        try container.encode(title, forKey: .title)
-        try container.encode(description, forKey: .description)
-        try container.encode(content, forKey: .content)
-        try container.encode(imageURL, forKey: .imageURL)
-        try container.encode(imageBackground, forKey: .imageBackground)
-        try container.encode(uiimage?.pngData(), forKey: .uiimage)
-    }
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        id = try container.decode(Int.self, forKey: .id)
-        title = try container.decode(String.self, forKey: .title)
-        description = try container.decode(String.self, forKey: .description)
-        content = try container.decode(String.self, forKey: .content)
-        imageURL = try container.decode(String.self, forKey: .imageURL)
-        imageBackground = try container.decode(Bool.self, forKey: .imageBackground)
-        uiimage = try container.decode(UIImage.self, forKey: .uiimage)
     }
 }
